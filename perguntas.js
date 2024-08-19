56e88a7456485be456d4f425d2c0c38a66508f03
Felipe <111072019+RexFelipe@users.noreply.github.com>
2024-08-19 12:38:50 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,22 @@
+export const perguntas = [
+    {
+        enunciado: "Como seria viver em um mundo dominado por inteligencia virtual, tipo exterminador do futuro?",
+        alternativas: [
+            {
+                texto: "Isso é assustador!",
+                afirmacao: [
+                    "Tentaria viver mesmo com a alta technologia dominando tudo e podendo morrer a qualquer momento. ",
+                    "Ou. Tentaria matar a IA com algum tipo de virus mesmo que isso te mate, mas o planeta seria salvo."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Viveria em guerra com a IA por sobrevivencia",
+                afirmacao: [
+                    "Mas os alimentos são escasos e não a comida para todo os sobreviventes.",
+                    "E suportar a saudade seria muito reduzido, pela falta de medicamentos, higiene e medicos."
+                ]
+            }
+        ]
+    }
+]    
